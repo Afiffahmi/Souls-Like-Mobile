9d831267f43ebdf03f9d279583e2be8e4c1030d7
Afif Fahmi <afif.rafpi@gmail.com>
2024-02-01 19:04:25 +0800

    BS Attack

--- Assets/Boss_Run.cs
+++ Assets/Boss_Run.cs
@@ -7,12 +7,16 @@ public class Boss_Run : StateMachineBehaviour
     public float speed = 2.5f;
     Transform player;
     Rigidbody rb;
+    Boss boss;
+    public float attackRange = 3f;
+    public float chaseRange = 10f;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         rb = animator.GetComponent<Rigidbody>();
+        boss = animator.GetComponent<Boss>();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
@@ -23,10 +27,21 @@ public class Boss_Run : StateMachineBehaviour
             return;
         }
 
+        boss.LookAtPlayer();
+
     
         Vector3 target = new Vector3(player.position.x, rb.position.y, rb.position.z);
         Vector3 newPos = Vector3.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
         rb.MovePosition(newPos);
+
+        if (Vector3.Distance(player.position, rb.position) < attackRange)
+        {
+            animator.SetTrigger("LightAttack");
+        }
+        else if (Vector3.Distance(player.position, rb.position) > chaseRange)
+        {
+            animator.SetTrigger("Idle");
+        }
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
