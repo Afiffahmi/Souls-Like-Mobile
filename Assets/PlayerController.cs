6927602318ab5476cd55c766f54e3548322dcfdc
Muhammad Hazim Bin Zamri <hzimzmri@gmail.com>
2024-02-03 10:19:45 +0800

    Revert "commit attack"
    This reverts commit 917feab1a3892132fd6b68a3838d432838f1d118.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -10,36 +10,26 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _movespeed, rotationSpeed;
     [SerializeField] private Animator _animator;
 
-    private bool canMove = true; // Flag to control movement
-
     private void FixedUpdate()
     {
-        if (canMove)
+        Vector3 moveDirection = new Vector3(_joystick.Horizontal, 0f, _joystick.Vertical).normalized;
+
+        if (moveDirection != Vector3.zero)
         {
-            Vector3 moveDirection = new Vector3(_joystick.Horizontal, 0f, _joystick.Vertical).normalized;
+            float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref rotationSpeed, 0.1f);
 
-            if (moveDirection != Vector3.zero)
-            {
-                float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
-                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref rotationSpeed, 0.1f);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-                transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-                _animator.SetTrigger("Running");
-                _animator.ResetTrigger("Idle");
-            }
-            else
-            {
-                _animator.SetTrigger("Idle");
-                _animator.ResetTrigger("Running");
-            }
-
-            _rigidbody.velocity = moveDirection * _movespeed;
+            _animator.SetTrigger("Running");
+            _animator.ResetTrigger("Idle");
         }
-    }
+        else
+        {
+            _animator.SetTrigger("Idle");
+            _animator.ResetTrigger("Running");
+        }
 
-    public void SetCanMove(bool canMoveValue)
-    {
-        canMove = canMoveValue;
+        _rigidbody.velocity = moveDirection * _movespeed;
     }
 }
